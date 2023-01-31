0e9a88a13ed26ef6c04bb50defa8b88d3f3feedf
Kilian PANOT <Kilian.PANOT@amadeus.com>
2023-01-31 19:42:08 +0100

    feat(vscode-extension): styling extractor
    Co-authored-by: @JasonHaenlinAmadeus
    Co-authored-by: @RubenHouri

---
+++ apps/vscode-extension/src/commands/extract/styling/extract-all-to-variable.command.ts
@@ -0,0 +1,59 @@
+import { EOL } from 'node:os';
+import { ExtensionContext, Position, Range, TextEditor, window } from 'vscode';
+import { generateVariableName, insertVariable, isPropertyValue } from './helpers';
+
+/**
+ * Replace all the interested lines with an Otter variable
+ *
+ * @param _context
+ */
+export function extractAllToVariable(_context: ExtensionContext) {
+  return async (editor: TextEditor) => {
+    const isPropertyLineRegExp = /^ *[^ .$#:]+ *: *([^$:]+) *;/;
+    const document = editor.document;
+    const lines: { valueRange: Range; variableName: string; value: string }[] = [];
+    for (let i = 0; i < document.lineCount; i++) {
+      const line = document.lineAt(i);
+      const match = line.text.match(isPropertyLineRegExp);
+      if (!match) {
+        continue;
+      }
+      const valueRange = new Range(
+        new Position(line.lineNumber, line.text.indexOf(match[1])),
+        new Position(line.lineNumber, line.text.indexOf(match[1]) + match[1].length)
+      );
+      if (!valueRange) {
+        continue;
+      }
+
+      const value = document.getText(valueRange);
+      if (!isPropertyValue(value, line)) {
+        continue;
+      }
+
+      const variableName = generateVariableName(document, line);
+
+      lines.push({
+        valueRange,
+        variableName,
+        value
+      });
+    }
+
+
+    const hasEdited = await editor.edit((edit) => {
+      let pos: Position | undefined;
+      lines.forEach(({valueRange, variableName, value}, idx) => {
+        edit.replace(valueRange, `$${variableName}`);
+        pos = insertVariable(document, edit, variableName, value, !!idx, pos);
+      });
+      if (pos && !document.lineAt(pos.line + 1).text.startsWith('$')) {
+        edit.insert(pos, EOL);
+      }
+    });
+
+    if (!hasEdited) {
+      await window.showInformationMessage(`The edition of ${document.fileName} has failed`);
+    }
+  };
+}
---
+++ docs/vscode-extension/EXTRACT_STYLING.md
@@ -0,0 +1,63 @@
+# Otter Extract Styling to o3r.variable
+
+Add commands to extract scss properties into Otter variables
+
+## Features
+
+### Extract a single variable
+
+![](../../.attachments/dev-tools/vscode-extensions/gif/extract-to-o3r-var.gif)
+
+### Extract all variables
+
+![](../../.attachments/dev-tools/vscode-extensions/gif/extract-all-to-o3r-var.gif)
+
+### Commands
+
+- `Otter: Extract SCSS property to o3r.variable` : extract a single pair property/value into an otter variable
+
+> must be on the line on which you want to perform the command
+
+- `Otter: Extract all SCSS properties to o3r.variable` : extract all the matches properties and values into otter variables
+
+### Extension Settings
+
+This extension can be configured the following settings:
+
+- `otter.extract.styling.prefix`: If not empty, will add a prefix to all the generated variables.
+
+## Example
+
+Before
+
+```scss
+// in app-header.scss file
+.app-header-class {
+  .title-text {
+    color: red;
+  }
+}
+```
+
+Use the command `Otter: Extract SCSS property to o3r-var` where the cursor is set
+> must be on the line on which you want to perform the command
+
+After
+
+```scss
+// in app-header.scss file
+@use "@o3r/styling" as o3r;
+$app-header-title-text-color: o3r.variable("app-header-title-text-color", red);
+
+.app-header-class {
+    .title-text {
+        color: $app-header-title-text-color;
+    }
+}
+```
+
+with the following configuration
+
+```json5
+"otter.extract.styling.prefix": "app" // default: ""
+```
