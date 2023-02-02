59ef6750803e28ad1874ea2fb41c90c7ff9c229d
Matthieu CROUZET <matthieu.crouzet@amadeus.com>
2023-02-02 16:14:16 +0100

    feat: add ng add for ama-sdk-core

--- packages/@ama-sdk/core/schematics/ng-add/index.ts
+++ packages/@ama-sdk/core/schematics/ng-add/index.ts
@@ -1,4 +1,6 @@
-import type { Rule } from '@angular-devkit/schematics';
+import type { Rule, Tree } from '@angular-devkit/schematics';
+import { getFilesInFolderFromWorkspaceProjectsInTree } from '@o3r/schematics';
+import * as ts from 'typescript';
 
 /**
  * Add Otter ama-sdk-core to an Angular Project
@@ -7,5 +9,27 @@ import type { Rule } from '@angular-devkit/schematics';
  */
 export function ngAdd(): Rule {
   /* ng add rules */
-  return () => {};
+  return (tree: Tree) => {
+    const files = getFilesInFolderFromWorkspaceProjectsInTree(tree, '', 'ts');
+    files.forEach((file) => {
+      const sourceFile = ts.createSourceFile(
+        file,
+        tree.readText(file),
+        ts.ScriptTarget.ES2015,
+        true
+      );
+      const dapiSdkCodeImports = sourceFile.statements.filter((node): node is ts.ImportDeclaration =>
+        ts.isImportDeclaration(node)
+        && !!node.moduleSpecifier.getText().match(/['"]@dapi\/sdk-core['"]/)
+      );
+      if (dapiSdkCodeImports.length) {
+        const recorder = tree.beginUpdate(file);
+        dapiSdkCodeImports.forEach((imp) => {
+          recorder.remove(imp.moduleSpecifier.getStart(), imp.moduleSpecifier.getWidth());
+          recorder.insertRight(imp.moduleSpecifier.getStart(), '\'@ama-sdk/core\'');
+        });
+        tree.commitUpdate(recorder);
+      }
+    });
+  }
 }
