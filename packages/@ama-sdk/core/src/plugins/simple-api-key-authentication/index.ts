b4852636d487b3d53f061901d6d5fa4e5c7ecd12
Jeremy BOURGEOIS <jeremy.bourgeois@amadeus.com>
2023-04-04 14:34:54 +0200

    feat: add plugin for new simple api key authentication on apigee

---
+++ packages/@ama-sdk/core/src/plugins/simple-api-key-authentication/index.ts
@@ -0,0 +1,1 @@
+export * from './simple-api-key-authentication.request';
---
+++ packages/@ama-sdk/core/src/plugins/simple-api-key-authentication/simple-api-key-authentication.spec.ts
@@ -0,0 +1,132 @@
+import { RequestOptions } from '../core/request-plugin';
+import { SimpleApiKeyAuthenticationRequest } from './simple-api-key-authentication.request';
+import {createBase64UrlDecoder} from '../../utils/json-token';
+
+const base64UrlDecoder = createBase64UrlDecoder();
+
+function getJWTPayload(jwt: string) {
+  return JSON.parse(base64UrlDecoder(jwt.split('.')[1]));
+}
+
+function returnInSequence<T>(...values: T[]) {
+  let index = 0;
+  return () => values[index++];
+}
+
+describe('Api Key Request Plugin', () => {
+
+  let options: RequestOptions;
+
+  beforeEach(() => {
+    const headers = new Headers();
+    options = {headers, basePath: 'http://test.com'};
+  });
+
+  it('Static API key should be added to the default header.', async () => {
+    const plugin = new SimpleApiKeyAuthenticationRequest('key');
+    const runner = plugin.load();
+
+    await runner.transform(options);
+
+    expect(options.headers.get('x-api-key')).toBe('key');
+  });
+
+  it('Static API key should be added to the specified header.', async () => {
+    const plugin = new SimpleApiKeyAuthenticationRequest('key', {
+      apiKeyHeader: 'custom-header'
+    });
+    const runner = plugin.load();
+
+    await runner.transform(options);
+
+    expect(options.headers.get('custom-header')).toBe('key');
+    expect(options.headers.get('x-api-key')).toBeNull();
+  });
+
+  it('Static OID override should compute the right ama-ctx', async () => {
+    const plugin = new SimpleApiKeyAuthenticationRequest('key', {
+      officeId: 'TESTOID'
+    });
+    const runner = plugin.load();
+
+    await runner.transform(options);
+    const amaCtx = options.headers.get('ama-ctx');
+
+    expect(amaCtx).toBeDefined();
+    expect(getJWTPayload(amaCtx)).toEqual(expect.objectContaining({
+      officeId: 'TESTOID'
+    }));
+  });
+
+  it('API key and OID as functions that return strings should result in the right headers', async () => {
+    const plugin = new SimpleApiKeyAuthenticationRequest(returnInSequence('key', 'secondKey'), {
+      officeId: returnInSequence('TESTOID', 'SECONDOID')
+    });
+    const runner = plugin.load();
+
+    await runner.transform(options);
+    let amaCtx = options.headers.get('ama-ctx');
+
+    expect(options.headers.get('x-api-key')).toBe('key');
+    expect(amaCtx).toBeDefined();
+    expect(getJWTPayload(amaCtx)).toEqual(expect.objectContaining({
+      officeId: 'TESTOID'
+    }));
+
+    await runner.transform(options);
+    amaCtx = options.headers.get('ama-ctx');
+
+    expect(options.headers.get('x-api-key')).toBe('secondKey');
+    expect(amaCtx).toBeDefined();
+    expect(getJWTPayload(amaCtx)).toEqual(expect.objectContaining({
+      officeId: 'SECONDOID'
+    }));
+  });
+
+  it('API key and OID as functions that return promises should result in the right headers', async () => {
+    const plugin = new SimpleApiKeyAuthenticationRequest(returnInSequence(Promise.resolve('key'), Promise.resolve('secondKey')), {
+      officeId: returnInSequence(Promise.resolve('TESTOID'), Promise.resolve('SECONDOID'))
+    });
+    const runner = plugin.load();
+
+    await runner.transform(options);
+    let amaCtx = options.headers.get('ama-ctx');
+
+    expect(options.headers.get('x-api-key')).toBe('key');
+    expect(amaCtx).toBeDefined();
+    expect(getJWTPayload(amaCtx)).toEqual(expect.objectContaining({
+      officeId: 'TESTOID'
+    }));
+
+    await runner.transform(options);
+    amaCtx = options.headers.get('ama-ctx');
+
+    expect(options.headers.get('x-api-key')).toBe('secondKey');
+    expect(amaCtx).toBeDefined();
+    expect(getJWTPayload(amaCtx)).toEqual(expect.objectContaining({
+      officeId: 'SECONDOID'
+    }));
+  });
+
+  it('Setting API key and OID using the setters should result in the right headers', async () => {
+    const plugin = new SimpleApiKeyAuthenticationRequest('key');
+    const runner = plugin.load();
+
+    await runner.transform(options);
+    let amaCtx = options.headers.get('ama-ctx');
+
+    expect(options.headers.get('x-api-key')).toBe('key');
+    expect(amaCtx).toBeNull();
+
+    plugin.setApiKey('secondApiKey');
+    plugin.setOfficeId('OID');
+    await runner.transform(options);
+    amaCtx = options.headers.get('ama-ctx');
+
+    expect(options.headers.get('x-api-key')).toBe('secondApiKey');
+    expect(amaCtx).toBeDefined();
+    expect(getJWTPayload(amaCtx)).toEqual(expect.objectContaining({
+      officeId: 'OID'
+    }));
+  });
+});
