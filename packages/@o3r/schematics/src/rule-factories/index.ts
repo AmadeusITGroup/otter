1a872b3999e0356410747a34b1d992c91ffef7aa
Kilian Panot <kilian.panot@amadeus.com>
2023-04-06 17:00:26 +0900

    fix: the way the update import functions are exposed

--- packages/@o3r/schematics/src/rule-factories/index.ts
+++ packages/@o3r/schematics/src/rule-factories/index.ts
@@ -4,4 +4,3 @@ export * from './check-packages-peers/index';
 export * from './ng-add/index';
 export * from './remove-packages/index';
 export * from './update-imports/index';
-export * from './updates-of-old-otter-scope/imports/update-imports-with-scope';
