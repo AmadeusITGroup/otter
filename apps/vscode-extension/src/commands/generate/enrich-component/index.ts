401b9e156c21c4278adf53be3c963841b1ddb93e
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2023-08-10 11:33:54 +0200

    feat(vscode ext): new command to add localization from the text selection in the template file

--- apps/vscode-extension/src/commands/generate/enrich-component/index.ts
+++ apps/vscode-extension/src/commands/generate/enrich-component/index.ts
@@ -4,6 +4,7 @@ export { generateAddContextToComponentCommand } from './context-to-component.com
 export { generateConvertComponentCommand } from './convert-component.commands';
 export { generateAddFixtureToComponentCommand } from './fixture-to-component.commands';
 export { generateAddIframeToComponentCommand } from './iframe-to-component.commands';
+export { generateAddLocalizationKeyToComponentCommand } from './localization-key-to-component.commands';
 export { generateAddLocalizationToComponentCommand } from './localization-to-component.commands';
 export { generateAddRulesEngineToComponentCommand } from './rules-engine-to-component.commands';
 export { generateAddThemingToComponentCommand } from './theming-to-component.commands';
