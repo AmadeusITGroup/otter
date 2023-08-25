ca5e58e0b8f98c615681035a5cd7296af645a4cd
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2023-08-25 17:29:45 +0200

    feat: schematic to generate facts service

---
+++ packages/@o3r/rules-engine/schematics/facts-service/schema.ts
@@ -0,0 +1,15 @@
+import type { SchematicOptionObject } from '@o3r/schematics';
+
+export interface NgGenerateFactsServiceSchematicsSchema extends SchematicOptionObject {
+  /** Project name */
+  projectName?: string | undefined;
+
+  /** Facts services Folder */
+  path: string;
+
+  /** Fact service name */
+  name: string;
+
+  /** Skip the linter process */
+  skipLinter: boolean;
+}
