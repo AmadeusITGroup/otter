6e5422e66e9492c2cc1145edfbc9742332147c6f
Kilian Panot <kilian.panot@amadeus.com>
2023-10-11 17:56:12 +0900

    feat: remove Otter theme to default export to remove Angular dep

---
+++ packages/@o3r/styling/schematics/ng-update/v10.0/index.spec.ts
@@ -0,0 +1,80 @@
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { updateScssImports } from './update-scss-imports';
+import * as path from 'node:path';
+import { lastValueFrom } from 'rxjs';
+
+describe('Update v10', () => {
+  const collectionPath = path.join(__dirname, '../../../migration.json');
+  let initialTree: UnitTestTree;
+
+  beforeEach(() => {
+    initialTree = new UnitTestTree(Tree.empty());
+  });
+
+  describe('Update SCSS Import', () => {
+
+    it('should not update a TS file', async () => {
+      const content = '@use "@o3r/styling" as o3r;\n';
+      initialTree.create('/source.ts', content);
+      const runner = new SchematicTestRunner('migrations', collectionPath);
+      const tree = await lastValueFrom(runner.callRule(updateScssImports(), initialTree));
+
+      expect(tree.readText('/source.ts')).toEqual(content);
+    });
+
+    it('should not update a file without Otter dependency', async () => {
+      const content = '@use "@angular/component" as o3r;\n';
+      initialTree.create('/style.scss', content);
+      const runner = new SchematicTestRunner('migrations', collectionPath);
+      const tree = await lastValueFrom(runner.callRule(updateScssImports(), initialTree));
+
+      expect(tree.readText('/style.scss')).toEqual(content);
+    });
+
+    it('should not update a file with Otter import with theme function only', async () => {
+      const content = `@use "@o3r/styling" as o3r;
+        @include o3r.var(#fff);`;
+      initialTree.create('/style.scss', content);
+      const runner = new SchematicTestRunner('migrations', collectionPath);
+      const tree = await lastValueFrom(runner.callRule(updateScssImports(), initialTree));
+
+      expect(tree.readText('/style.scss')).toEqual(content);
+    });
+
+    it('should update a file with Otter theme usage', async () => {
+      const content = `
+        @use "@o3r/styling" as o3r;
+        @include o3r.var(#fff);
+        $var: o3r.meta-theme-to-otter();`;
+      const expected = `@use '@o3r/styling/otter-theme' as otter-theme;
+        @use "@o3r/styling" as o3r;
+        @include o3r.var(#fff);
+        $var: otter-theme.meta-theme-to-otter();`;
+      initialTree.create('/style.scss', content);
+      const runner = new SchematicTestRunner('migrations', collectionPath);
+      const tree = await lastValueFrom(runner.callRule(updateScssImports(), initialTree));
+      const res = tree.readText('/style.scss').replaceAll(/^ +$/gm, '').replaceAll(/\n\n/g, '\n');
+
+      expect(res).toEqual(expected);
+    });
+
+    it('should update a file with Otter theme usage with non-standard alias', async () => {
+      const content = `@use "@o3r/styling" as test-name;
+        $var: test-name.meta-theme-to-otter();`;
+      const expected = `@use '@o3r/styling/otter-theme' as otter-theme;
+        $var: otter-theme.meta-theme-to-otter();`;
+      initialTree.create('/style.scss', content);
+      const runner = new SchematicTestRunner('migrations', collectionPath);
+      const tree = await lastValueFrom(runner.callRule(updateScssImports(), initialTree));
+      const res = tree.readText('/style.scss').replaceAll(/^ +$/gm, '').replaceAll(/\n\n/g, '\n');
+
+      expect(res).toEqual(expected);
+    });
+
+    it('should update a file with Otter theme usage and clean previous dep', () => {
+
+    });
+
+  });
+});
---
+++ packages/@o3r/styling/schematics/ng-update/v10.0/update-scss-imports.ts
@@ -0,0 +1,60 @@
+import type { Rule } from '@angular-devkit/schematics';
+import { getFilesWithExtensionFromTree } from '@o3r/schematics';
+
+export const updateScssImports = (): Rule => {
+  const otterThemeFunctions = [
+    'meta-theme-to-otter',
+    'generate-theme-variables',
+    'generate-otter-theme',
+    'generate-otter-dark-theme',
+    'revert-palette',
+    'generate-theme',
+    'meta-theme-to-material',
+    'get',
+    'color',
+    'contrast',
+    '$default-meta-theme',
+    '$default-theme',
+    '$default-mat-theme'
+  ];
+
+  const otterSubEntryPrefix = 'otter-theme';
+
+  return (tree) => {
+    const files = getFilesWithExtensionFromTree(tree, 'scss');
+    files
+      .map((file) => {
+        const content = tree.readText(file);
+        const match = content.match(/@use +['"]@o3r\/styling['"] +as +(.*);/);
+        const importName = match?.[1];
+        if (!importName) {
+          return {
+            file,
+            importName,
+            content,
+            toReplace: undefined
+          };
+        }
+        return {
+          file,
+          importName,
+          content,
+          toReplace: otterThemeFunctions
+            .map((item) => ({ from: `${importName}.${item}`, to: `${otterSubEntryPrefix}.${item}` }))
+            .filter(({from}) => content.indexOf(from) >= 0)
+        };
+      })
+      .filter(({ toReplace, importName }) => !!importName && !!toReplace?.length)
+      .forEach(({ file, content, toReplace, importName }) => {
+        let newContent = `@use '@o3r/styling/otter-theme' as ${otterSubEntryPrefix};\n` +
+          toReplace!.reduce((acc, {from, to}) => {
+            return acc.replaceAll(from, to);
+          }, content);
+        if (newContent.indexOf(`${importName!}.`) === -1) {
+          newContent = newContent.replace(new RegExp(`@use +['"]@o3r/styling['"] +as +${importName!};\n?`), '');
+        }
+        tree.overwrite(file, newContent);
+      });
+    return tree;
+  };
+};
