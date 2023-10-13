ae30e21686f4b83cf3ee41d0e6b08a5c83722fd4
Corinne PAULVE <corinne.paulve@amadeus.com>
2023-10-13 10:36:06 +0200

    feat: post message events upon placeholder

---
+++ packages/@o3r/components/src/tools/placeholder/placeholder.interface.ts
@@ -0,0 +1,23 @@
+import {OtterMessageContent} from '@o3r/core';
+
+/**
+ * Payload of the {@see PlaceholderLoadingStatusMessage}
+ *
+ * Describe the state of an identified placeholder: what template is being loaded and whether the load is done
+ */
+export interface PlaceholderLoadingStatus {
+  /**
+   * Ids of the template to be loaded in the placeholder
+   */
+  templateIds?: string[];
+  /**
+   * Identify the placeholder under consideration
+   */
+  placeholderId?: string;
+}
+
+/**
+ * Message to describe a placeholder's loading status: the templates to be loaded and the pending status.
+ */
+export interface PlaceholderLoadingStatusMessage extends PlaceholderLoadingStatus,
+  OtterMessageContent<'placeholder-loading-status'> {}
