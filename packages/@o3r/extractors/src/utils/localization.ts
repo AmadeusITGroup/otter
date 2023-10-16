36c1ac092f7ad3116dc88740c6b493a2b4c37fc4
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2023-10-16 18:25:17 +0200

    feat(extractor): add mapping between localization key and component

---
+++ packages/@o3r/extractors/src/utils/localization.ts
@@ -0,0 +1,37 @@
+import * as ts from 'typescript';
+
+const localizationDecoratorName = 'Localization';
+
+/**
+ * Retrieve the localization json files from TS Code
+ *
+ * @param node TSNode of the angular component class
+ */
+export function getLocalizationFileFromAngularElement(node: ts.ClassDeclaration): string[] | undefined {
+  const localizationPaths: string[] = [];
+  node.forEachChild((item) => {
+    if (!ts.isPropertyDeclaration(item)) {
+      return;
+    }
+
+    item.forEachChild((decorator) => {
+      if (
+        !ts.isDecorator(decorator)
+        || !ts.isCallExpression(decorator.expression)
+        || !ts.isIdentifier(decorator.expression.expression)
+        || decorator.expression.expression.escapedText !== localizationDecoratorName
+      ) {
+        return;
+      }
+
+      const firstArg = decorator.expression.arguments[0];
+      if (!firstArg || !ts.isStringLiteral(firstArg)) {
+        return;
+      }
+
+      localizationPaths.push(firstArg.text);
+    });
+  });
+
+  return localizationPaths.length ? localizationPaths : undefined;
+}
