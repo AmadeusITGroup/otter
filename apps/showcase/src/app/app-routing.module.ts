3b82e7c92643c669633b8f333ad5889b7f296ff1
Florian PAUL <florian.paul@amadeus.com>
2023-10-25 13:57:55 +0200

    feat(showcase): design-factory integration

--- apps/showcase/src/app/app-routing.module.ts
+++ apps/showcase/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, {scrollPositionRestoration: 'enabled'})
+    RouterModule.forRoot(appRoutes, {scrollPositionRestoration: 'enabled', useHash: true})
   ],
   exports: [RouterModule]
 })
