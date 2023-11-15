e45448f6cfc8ea69da981cdd6793d2b6db4c123d
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2023-11-15 14:10:06 +0100

    feat(component): generate a simple component by default

--- packages/@o3r/core/schematics/component/structures.types.ts
+++ packages/@o3r/core/schematics/component/structures.types.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-export type ComponentStructure = 'full' | 'container' | 'presenter';
+export type ComponentStructure = 'full' | 'container' | 'presenter' | 'simple';
 export enum ComponentStructureDef {
   'Cont' = 'Cont',
-  'Pres' = 'Pres'
+  'Pres' = 'Pres',
+  'Simple' = 'Simple'
 }
