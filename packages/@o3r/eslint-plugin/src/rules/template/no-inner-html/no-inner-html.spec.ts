87ebabcb6653cbfcf370ebbfbb40de18a6ff2687
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2023-11-23 14:07:08 +0100

    feat(linter): new template rule to detect innerhtml

---
+++ packages/@o3r/eslint-plugin/src/rules/template/no-inner-html/no-inner-html.spec.ts
@@ -0,0 +1,32 @@
+import { RuleTester } from '@angular-eslint/utils';
+import noInnerHTMLRule, { name } from './no-inner-html';
+
+const ruleTester = new RuleTester({
+  parser: '@angular-eslint/template-parser'
+});
+
+const validHTML = '<p innerText="test"></p>';
+
+ruleTester.run(name, noInnerHTMLRule as any /* workaround for 5.9.0 breaking change on interface */, {
+  valid: [validHTML],
+  invalid: [
+    {
+      code: validHTML.replace('innerText', 'innerHTML'),
+      output: validHTML,
+      errors: [
+        {
+          messageId: 'error'
+        }
+      ]
+    },
+    {
+      code: validHTML.replace('innerText', 'innerHtml'),
+      output: validHTML,
+      errors: [
+        {
+          messageId: 'error'
+        }
+      ]
+    }
+  ]
+});
