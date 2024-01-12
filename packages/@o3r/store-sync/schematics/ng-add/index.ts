3a376a7cbd3438909242802191032252cc040360
Kilian Panot <kilian.panot@amadeus.com>
2024-01-13 00:12:29 +0900

    fix: add missing peer dependencies to ngAdd Schematics

--- packages/@o3r/store-sync/schematics/ng-add/index.ts
+++ packages/@o3r/store-sync/schematics/ng-add/index.ts
@@ -1,6 +1,7 @@
 import { chain, noop, Rule } from '@angular-devkit/schematics';
 import type { NgAddSchematicsSchema } from './schema';
 import * as path from 'node:path';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 /**
  * Add Otter store-sync to an Otter Project
@@ -10,7 +11,7 @@ export function ngAdd(options: NgAddSchematicsSchema): Rule {
   return async (tree, context) => {
     try {
       // use dynamic import to properly raise an exception if it is not an Otter project.
-      const { applyEsLintFix, install, ngAddPackages, getO3rPeerDeps, getWorkspaceConfig } = await import('@o3r/schematics');
+      const { applyEsLintFix, install, ngAddPackages, getO3rPeerDeps, getWorkspaceConfig, getPeerDepWithPattern } = await import('@o3r/schematics');
       // retrieve dependencies following the /^@o3r\/.*/ pattern within the peerDependencies of the current module
       const depsInfo = getO3rPeerDeps(path.resolve(__dirname, '..', '..', 'package.json'));
       const workingDirectory = options?.projectName && getWorkspaceConfig(tree)?.projects[options.projectName]?.root || '.';
@@ -26,7 +27,20 @@ export function ngAdd(options: NgAddSchematicsSchema): Rule {
           parentPackageInfo: `${depsInfo.packageName!} - setup`,
           projectName: options.projectName,
           workingDirectory
-        })
+        }),
+
+        // Add mandatory peerDependency
+        (_, ctx) => {
+          const peerDepToInstall = getPeerDepWithPattern(path.resolve(__dirname, '..', '..', 'package.json'), ['fast-deep-equal']);
+          ctx.addTask(new NodePackageInstallTask({
+            packageName: Object.entries(peerDepToInstall.matchingPackagesVersions)
+              // eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
+              .map(([dependency, version]) => `${dependency}@${version || 'latest'}`)
+              .join(' '),
+            hideOutput: false,
+            quiet: false
+          }));
+        }
       ]);
     } catch (e) {
       // If the installation is initialized in a non-Otter application, mandatory packages will be missing. We need to notify the user
