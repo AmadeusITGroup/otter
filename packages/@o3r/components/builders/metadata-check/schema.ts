255b3bf3fda45b57dd5bef319a15c286fd038cc2
Clara POURCEL <clara.pourcel@amadeus.com>
2024-04-29 14:56:44 +0200

    feat: add new builders for metadata migration check

---
+++ packages/@o3r/components/builders/metadata-check/schema.ts
@@ -0,0 +1,5 @@
+import type { MigrationMetadataCheckBuilderOptions } from '@o3r/extractors';
+
+/** Migration metadata check builder schema */
+export interface ConfigMigrationMetadataCheckBuilderSchema extends MigrationMetadataCheckBuilderOptions {
+}
