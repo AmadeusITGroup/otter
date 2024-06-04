dcbbdac12de4a00f7b05ba2709586a5a538218c6
Corinne PAULVE <corinne.paulve@amadeus.com>
2024-06-04 17:46:54 +0200

    feat: expose current time fact service
    Expose in library the inner o3rCurrentTime fact and delete unexpose fact factory.
    
    Rename facts folder into inner-facts not to expose o3rCurrentTime in the fact metadata

--- packages/@o3r/rules-engine/src/inner-facts/current-time/current-time-fact.service.ts
+++ packages/@o3r/rules-engine/src/inner-facts/current-time/current-time-fact.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CurrentTimeFacts, FactsService, RulesEngineRunnerService } from '@o3r/rules-engine';
 import { BehaviorSubject } from 'rxjs';
+import { FactsService } from '../../fact';
+import { CurrentTimeFacts } from './current-time.facts';
+import { RulesEngineRunnerService } from '../../services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentTimeFactsService extends FactsService<CurrentTimeFacts> {
 
-  private readonly currentTimeSubject$ = new BehaviorSubject(new Date('2023-11-2').getTime());
+  private readonly currentTimeSubject$ = new BehaviorSubject(Date.now());
   /** @inheritDoc */
   public facts = {
     o3rCurrentTime: this.currentTimeSubject$.asObservable()
