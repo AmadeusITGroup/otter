352e89c64f2a02e274ae2289c2fec6df3135faa6
Corinne PAULVE <corinne.paulve@amadeus.com>
2024-07-26 11:43:54 +0200

    chore: wip

--- apps/showcase/src/app/sdk-training/sdk-training.component.ts
+++ apps/showcase/src/app/sdk-training/sdk-training.component.ts
@@ -1,38 +1,43 @@
-import { Component } from '@angular/core';
-import {
-  CopyTextPresComponent,
-  SdkTrainingGenerationConfigPresComponent,
-  SdkTrainingGenerationOptionsPresComponent,
-  SdkTrainingGenerationSetupExamplePresComponent,
-  SdkTrainingGenerationSetupPresComponent,
-  SdkTrainingGenerationStructurePresComponent,
-  SdkTrainingIntroPresComponent,
-  SdkTrainingWelcomePresComponent
-} from '../../components';
+import {Component, signal, ViewEncapsulation} from '@angular/core';
+import {CopyTextPresComponent, SdkTrainingStepPresComponent,} from '../../components';
+import {AsyncPipe, JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'o3r-sdk-training',
   standalone: true,
   imports: [
+    AsyncPipe,
     CopyTextPresComponent,
-    SdkTrainingGenerationConfigPresComponent,
-    SdkTrainingGenerationOptionsPresComponent,
-    SdkTrainingGenerationSetupExamplePresComponent,
-    SdkTrainingGenerationSetupPresComponent,
-    SdkTrainingGenerationStructurePresComponent,
-    SdkTrainingIntroPresComponent,
-    SdkTrainingWelcomePresComponent
+    SdkTrainingStepPresComponent,
+    JsonPipe
   ],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './sdk-training.component.html',
   styleUrl: './sdk-training.component.scss'
 })
 export class SdkTrainingComponent {
-  public previousStep: boolean = true;
-  public nextStep: boolean = true;
   public currentStep: number;
-  constructor () {
+  public steps: any[] = [{
+    title: 'Introduction',
+    htmlContentUrl: 'sdk-training/step-1.html',
+    htmlContent: signal(''),
+    htmlExampleUrl: 'sdk-training/step-1.example.html',
+    htmlExample: signal('')
+  }];
+  public nextStep: boolean;
+  public previousStep: boolean;
+
+  constructor() {
     const stepRequested = window.location.href.match(new RegExp('#([0-9]+)'))?.[1];
     this.currentStep = !stepRequested || Number.isNaN(stepRequested) ? 0 : Number.parseInt(stepRequested);
+    this.previousStep = this.currentStep > 0;
+    this.nextStep = this.currentStep < this.steps.length - 1;
+    if (this.steps[this.currentStep] && !this.steps[this.currentStep].htmlContent()) {
+      void this.loadHtml(this.steps[this.currentStep].htmlContentUrl).then((content) => this.steps[this.currentStep].htmlContent.set(content));
+    }
+    if (this.steps[this.currentStep] && !this.steps[this.currentStep].htmlExample()) {
+      void this.loadHtml(this.steps[this.currentStep].htmlExampleUrl).then((content) => this.steps[this.currentStep].htmlExample.set(content));
+    }
   }
 
   public goToNextStep() {
@@ -40,9 +45,20 @@ export class SdkTrainingComponent {
       return;
     }
     this.currentStep++;
-    this.previousStep = this.currentStep !== 0;
-    this.nextStep = this.currentStep !== 6;
+    this.previousStep = this.currentStep > 0;
+    this.nextStep = this.currentStep < this.steps.length - 1;
     history.pushState(null, '', `#/sdk-training#${this.currentStep}`);
+    if (this.steps[this.currentStep] && !this.steps[this.currentStep].htmlContent()) {
+      void this.loadHtml(this.steps[this.currentStep].htmlContentUrl).then((content) => this.steps[this.currentStep].htmlContent.set(content));
+    }
+    if (this.steps[this.currentStep] && !this.steps[this.currentStep].htmlExample()) {
+      void this.loadHtml(this.steps[this.currentStep].htmlExampleUrl).then((content) => this.steps[this.currentStep].htmlExample.set(content));
+    }
+  }
+
+  public async loadHtml(url: string) {
+    const response = await fetch(`assets/${url}`);
+    return (await response.text());
   }
 
   public goToPreviousStep() {
@@ -52,6 +68,12 @@ export class SdkTrainingComponent {
     this.currentStep--;
     this.previousStep = this.currentStep !== 0;
     this.nextStep = this.currentStep !== 6;
+    if (this.steps[this.currentStep] && this.steps[this.currentStep].htmlContent()) {
+      void this.loadHtml(this.steps[this.currentStep].htmlContentUrl).then((content) => this.steps[this.currentStep].htmlContent.set(content));
+    }
+    if (this.steps[this.currentStep] && this.steps[this.currentStep].htmlExample()) {
+      void this.loadHtml(this.steps[this.currentStep].htmlExampleUrl).then((content) => this.steps[this.currentStep].htmlExample.set(content));
+    }
     history.pushState(null, '', `#/sdk-training#${this.currentStep}`);
   }
 }
