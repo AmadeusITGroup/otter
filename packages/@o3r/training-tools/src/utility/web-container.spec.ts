d6ba28561f08a51f0886764f43049cf640273af0
Salome DO <salome.do@amadeus.com>
2024-08-29 11:13:57 +0200

    feat: otter training - tools and services

---
+++ packages/@o3r/training-tools/src/utility/web-container.spec.ts
@@ -0,0 +1,7 @@
+describe('Otter Training WebContainer', () => {
+
+  // TODO create test for the exported getFilesTree function
+  it('should get the file tree from the given file system without the files/directories to exclude', () => {
+    expect(true).toBe(true);
+  });
+});
