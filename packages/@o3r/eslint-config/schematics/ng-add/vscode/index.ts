f1f4448c6be5df05833546cc429f68c874dc39e4
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2024-09-17 17:06:27 +0200

    feat(eslint): ng-add eslint-config

---
+++ packages/@o3r/eslint-config/schematics/ng-add/vscode/index.ts
@@ -0,0 +1,24 @@
+import type { JsonObject } from '@angular-devkit/core';
+import { chain, type Rule } from '@angular-devkit/schematics';
+
+/**
+ * Update VSCode recommendations and settings
+ */
+export const updateVscode: Rule = async () => {
+  const { addVsCodeRecommendations } = await import('@o3r/schematics');
+  return chain([
+    addVsCodeRecommendations(['dbaeumer.vscode-eslint', 'stylelint.vscode-stylelint']),
+    (tree) => {
+      const vscodeSettingsPath = '.vscode/settings.json';
+      const settings = tree.exists(vscodeSettingsPath) ? (tree.readJson(vscodeSettingsPath) || {}) as JsonObject : {};
+      settings['eslint.useFlatConfig'] = true;
+      settings['editor.defaultFormatter'] = 'dbaeumer.vscode-eslint';
+      if (tree.exists(vscodeSettingsPath)) {
+        tree.overwrite(vscodeSettingsPath, JSON.stringify(settings, null, 2));
+      } else {
+        tree.create(vscodeSettingsPath, JSON.stringify(settings, null, 2));
+      }
+      return tree;
+    }
+  ]);
+};
