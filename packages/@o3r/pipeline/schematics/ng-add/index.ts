15bdc009a953df5c760f7b47ce48daef8c380d55
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2024-11-07 10:29:25 +0100

    feat(eslint): migrate pipeline package

--- packages/@o3r/pipeline/schematics/ng-add/index.ts
+++ packages/@o3r/pipeline/schematics/ng-add/index.ts
@@ -1,16 +1,31 @@
-import { apply, chain, MergeStrategy, mergeWith, move, Rule, template, url } from '@angular-devkit/schematics';
-import type { NgAddSchematicsSchema } from './schema';
-import {dump, load} from 'js-yaml';
-import * as path from 'node:path';
 import * as fs from 'node:fs';
-import type { PackageJson } from 'type-fest';
+import * as path from 'node:path';
+import {
+  apply,
+  chain,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  template,
+  url
+} from '@angular-devkit/schematics';
+import {
+  dump,
+  load
+} from 'js-yaml';
+import type {
+  PackageJson
+} from 'type-fest';
+import type {
+  NgAddSchematicsSchema
+} from './schema';
 
 /**
  * Add an Otter CI pipeline to an Angular Project
  * @param options
  */
 function ngAddFn(options: NgAddSchematicsSchema): Rule {
-
   return async (tree, context) => {
     const packageJsonPath = path.resolve(__dirname, '..', '..', 'package.json');
     const ownPackageJson = JSON.parse(fs.readFileSync(packageJsonPath, { encoding: 'utf8' })) as PackageJson & { config?: { o3r?: { commitHash?: string } } };
@@ -43,18 +58,16 @@ function ngAddFn(options: NgAddSchematicsSchema): Rule {
       }
       if (packageManager === 'yarn') {
         const yarnrcPath = '/.yarnrc.yml';
-        if (!tree.exists(yarnrcPath)) {
-          tree.create(yarnrcPath, dump({'npmRegistryServer': options.npmRegistry}, {indent: 2}));
-        } else {
-          const yarnrcContent = load(tree.readText(yarnrcPath)) as any;
+        if (tree.exists(yarnrcPath)) {
+          const yarnrcContent = load(tree.readText(yarnrcPath)) as { npmRegistryServer: string };
           yarnrcContent.npmRegistryServer = options.npmRegistry;
-          tree.overwrite(yarnrcPath, dump(yarnrcContent, {indent: 2}));
+          tree.overwrite(yarnrcPath, dump(yarnrcContent, { indent: 2 }));
+        } else {
+          tree.create(yarnrcPath, dump({ npmRegistryServer: options.npmRegistry }, { indent: 2 }));
         }
       } else if (packageManager === 'npm') {
         const npmrcPath = '/.npmrc';
-        if (!tree.exists(npmrcPath)) {
-          tree.create(npmrcPath, `registry=${options.npmRegistry}`);
-        } else {
+        if (tree.exists(npmrcPath)) {
           const npmrcContent = tree.readText(npmrcPath);
           const registryPattern = /^registry=.*$/m;
           const newRegistryLine = `registry=${options.npmRegistry}`;
@@ -62,6 +75,8 @@ function ngAddFn(options: NgAddSchematicsSchema): Rule {
             ? npmrcContent.replace(registryPattern, newRegistryLine)
             : `${npmrcContent}\n${newRegistryLine}`;
           tree.overwrite(npmrcPath, newContent);
+        } else {
+          tree.create(npmrcPath, `registry=${options.npmRegistry}`);
         }
       }
       return tree;
@@ -72,7 +87,6 @@ function ngAddFn(options: NgAddSchematicsSchema): Rule {
       npmRegistryRule
     ];
     return () => chain(rules)(tree, context);
-
   };
 }
 
