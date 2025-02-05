65dc0664ec1a5f34d4ed8959b807cb2397c71886
mrednic <mircea.rednic@amadeus.com>
2025-02-05 11:20:45 +0100

    feat: communication protocol

---
+++ packages/@ama-mfe/messages/src/messages/navigation/index.ts
@@ -0,0 +1,9 @@
+import type {
+  Navigation as NavigationV1_0,
+} from './navigation.1.0';
+
+export type { Navigation as NavigationV1_0 } from './navigation.1.0';
+export { MESSAGE_NAVIGATION_TYPE } from './base';
+
+/** The versions of navigation messages */
+export type NavigationVersions = NavigationV1_0;
