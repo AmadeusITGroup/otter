2a500e1949b4e94d1e37f64e43e6b4775016069c
vscaiceanu-1a <86055112+vscaiceanu-1a@users.noreply.github.com>
2025-03-03 11:53:25 +0100

    fix: forward options for presets

--- packages/@o3r/core/schematics/ng-add/index.ts
+++ packages/@o3r/core/schematics/ng-add/index.ts
@@ -85,8 +85,9 @@ function ngAddFn(options: NgAddSchematicsSchema): Rule {
       registerPackageCollectionSchematics(corePackageJsonContent),
       async (t, c) => {
         const { preset, externalPresets, ...forwardOptions } = options;
-        const presetRunner = await presets[preset]({ projectName: forwardOptions.projectName, forwardOptions });
-        const externalPresetRunner = externalPresets ? await getExternalPreset(externalPresets, t, c)({ projectName: forwardOptions.projectName, forwardOptions }) : undefined;
+        const presetOptions = { projectName: forwardOptions.projectName, exactO3rVersion: forwardOptions.exactO3rVersion, forwardOptions };
+        const presetRunner = await presets[preset](presetOptions);
+        const externalPresetRunner = externalPresets ? await getExternalPreset(externalPresets, t, c)(presetOptions) : undefined;
         const modules = [...new Set([...(presetRunner.modules || []), ...(externalPresetRunner?.modules || [])])];
         if (modules.length > 0) {
           c.logger.info(`The following modules will be installed: ${modules.join(', ')}`);
