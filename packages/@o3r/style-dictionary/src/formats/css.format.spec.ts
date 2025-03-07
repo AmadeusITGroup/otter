1250b46124333f85b595166101b42ce168a5eb01
Kilian Panot <kilian.panot@amadeus.com>
2025-03-08 01:24:26 +0900

    fix(style-dictionary): for corner case values fix(style-dictionary): fallback file for token not matching rules feat(style-dictionary): add support for gradient tokens

--- packages/@o3r/style-dictionary/src/formats/css.format.spec.ts
+++ packages/@o3r/style-dictionary/src/formats/css.format.spec.ts
@@ -14,6 +14,8 @@ import {
 const mocksPath = resolve(__dirname, '..', '..', 'testing', 'mocks', 'dictionaries');
 
 let mocks!: {
+  gradientToken: any;
+  gradientTokenRef: any;
   singleToken: any;
   privateToken: any;
   referenceToken: any;
@@ -21,7 +23,10 @@ let mocks!: {
 
 jest.mock('style-dictionary/utils', () => ({
   createPropertyFormatter: jest.fn().mockImplementation(() => ({ name, $value, original }: any) =>
-    original?.$value?.startsWith('{') ? `--${name}: var(--${original.$value.replace(/\{(.*)\}/, '$1').replaceAll('.', '-')})` : `--${name}: ${$value};`),
+    typeof original?.$value === 'string'
+      ? (original?.$value?.startsWith('{') ? `--${name}: var(--${original.$value.replace(/\{(.*)\}/, '$1').replaceAll('.', '-')})` : `--${name}: ${$value};`)
+      : `--${name}: ${original?.$value};`
+  ),
   fileHeader: jest.fn().mockReturnValue(''),
   getReferences: jest.fn().mockImplementation((value, tokens) => {
     const flat = (obj: any, mem: Set<any>) => {
@@ -33,7 +38,7 @@ jest.mock('style-dictionary/utils', () => ({
       }
       return mem;
     };
-    return [...flat(tokens, new Set())].filter(({ path }: any) => value?.includes(path.join('.')));
+    return [...flat(tokens, new Set())].filter(({ path }: any) => JSON.stringify(value)?.includes(path.join('.')));
   }),
   sortByReference: jest.fn()
 }));
@@ -45,7 +50,9 @@ describe('cssFormat', () => {
     mocks = {
       singleToken: JSON.parse(readFileSync(resolve(mocksPath, 'single-token.json'), { encoding: 'utf8' })),
       privateToken: JSON.parse(readFileSync(resolve(mocksPath, 'private-token.json'), { encoding: 'utf8' })),
-      referenceToken: JSON.parse(readFileSync(resolve(mocksPath, 'ref-token.json'), { encoding: 'utf8' }))
+      referenceToken: JSON.parse(readFileSync(resolve(mocksPath, 'ref-token.json'), { encoding: 'utf8' })),
+      gradientToken: JSON.parse(readFileSync(resolve(mocksPath, 'gradient-token.json'), { encoding: 'utf8' })),
+      gradientTokenRef: JSON.parse(readFileSync(resolve(mocksPath, 'gradient-token-ref.json'), { encoding: 'utf8' }))
     };
   });
 
@@ -61,6 +68,26 @@ describe('cssFormat', () => {
       platform: {}
     }) as string;
     expect(css.replaceAll(/[\r\n]/g, '')).toBe(':root {--color-primary-50: #ebf3ff;}');
+  });
+
+  test('should format gradient CSS variable', async () => {
+    const css = await cssFormat.format({
+      dictionary: mocks.gradientToken,
+      file: {},
+      options: { outputReferences: true, usesDtcg: true },
+      platform: {}
+    }) as string;
+    expect(css.replaceAll(/[\r\n]/g, '')).toBe(':root {--gradient-primary-50: linear-gradient(90deg, #000 0%, #fff 1%);}');
+  });
+
+  test('should format gradient CSS variable ref', async () => {
+    const css = await cssFormat.format({
+      dictionary: mocks.gradientTokenRef,
+      file: {},
+      options: { outputReferences: true, usesDtcg: true },
+      platform: {}
+    }) as string;
+    expect(css.replaceAll(/[\r\n]/g, '')).toBe(':root {--color-primary-50: #ebf3ff;--gradient-primary-50: linear-gradient(90deg, var(--color-primary-50) 0%, #fff 1%);}');
   });
 
   test('should format reference CSS variable', async () => {
