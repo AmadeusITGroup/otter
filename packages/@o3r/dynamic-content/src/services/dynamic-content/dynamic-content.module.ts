7ca8b5e895d478b7ad2d92c0ab148f227e5f0a64
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2025-03-14 08:59:47 +0100

    feat: propose provider instead of module.forRoot

--- packages/@o3r/dynamic-content/src/services/dynamic-content/dynamic-content.module.ts
+++ packages/@o3r/dynamic-content/src/services/dynamic-content/dynamic-content.module.ts
@@ -1,6 +1,9 @@
 import {
+  type EnvironmentProviders,
+  makeEnvironmentProviders,
   ModuleWithProviders,
   NgModule,
+  type Provider,
 } from '@angular/core';
 import {
   O3rDynamicContentPipe,
@@ -28,8 +31,11 @@ export function getCmsAssets() {
   return document.body.dataset.cmsassetspath || '';
 }
 
+/**
+ * DynamicContent module
+ * @deprecated Will be removed in v14.
+ */
 @NgModule({
-  declarations: [O3rDynamicContentPipe],
   providers: [
     {
       provide: DYNAMIC_CONTENT_BASE_PATH_TOKEN,
@@ -41,11 +47,8 @@ export function getCmsAssets() {
     },
     DynamicContentService
   ],
-  exports: [O3rDynamicContentPipe]
+  imports: [O3rDynamicContentPipe]
 })
-/**
- * DynamicContent module
- */
 export class DynamicContentModule {
   /**
    * Customize the location where the application will search for the base path of dynamic content
@@ -53,6 +56,7 @@ export class DynamicContentModule {
    * @param dynamicPath.content The string will be used as base path of dynamic content
    * @param dynamicPath.cmsAssets The string will be used for the the base path of cms assets
    * @note The cmsAssets will be used only in the cms editor mode and it will take priority over dynamic content
+   * @deprecated Please use {@link provideDynamicContent} instead. Will be removed in v14.
    */
   public static forRoot(dynamicPath: { content: string } | { cmsAssets: string }): ModuleWithProviders<DynamicContentModule> {
     const providers = [];
@@ -74,4 +78,90 @@ export class DynamicContentModule {
       providers
     };
   }
+}
+
+type DynamicContentFeatureKind = 'base-path' | 'cms-assets-path';
+
+interface DynamicContentFeature<FeatureKind extends DynamicContentFeatureKind> {
+  ɵkind: FeatureKind;
+  ɵproviders: Provider[];
+}
+
+type BasePathFeature = DynamicContentFeature<'base-path'>;
+type CmsAssetsPathFeature = DynamicContentFeature<'cms-assets-path'>;
+
+type DynamicContentFeatures = BasePathFeature | CmsAssetsPathFeature;
+
+/**
+ * Specify a custom base path
+ * @param basePath
+ */
+export function withBasePath(basePath: string | (() => string)): BasePathFeature {
+  return {
+    ɵkind: 'base-path',
+    ɵproviders: [
+      {
+        provide: DYNAMIC_CONTENT_BASE_PATH_TOKEN,
+        ...(
+          typeof basePath === 'string'
+            ? { useValue: basePath }
+            : { useFactory: basePath }
+        )
+      }
+    ]
+  };
+}
+
+/**
+ * Specify a custom CMS assets path
+ * @param cmsAssetsPath
+ */
+export function withCmsAssetsPath(cmsAssetsPath: string | (() => string)): DynamicContentFeature<'cms-assets-path'> {
+  return {
+    ɵkind: 'cms-assets-path',
+    ɵproviders: [
+      {
+        provide: CMS_ASSETS_PATH_TOKEN,
+        ...(
+          typeof cmsAssetsPath === 'string'
+            ? { useValue: cmsAssetsPath }
+            : { useFactory: cmsAssetsPath }
+        )
+      }
+    ]
+  };
+}
+
+/**
+ * Provide dynamic content default configuration.
+ * To customize the location where the application will search for the base path of dynamic content
+ * @see {@link withBasePath}
+ * @see {@link withCmsAssetsPath}
+ * @note The cmsAssets will be used only in the cms editor mode and it will take priority over dynamic content
+ * @param features
+ * @example
+ * ```typescript
+ * bootstrapApplication(AppComponent,
+ *  {
+ *    providers: [
+ *      provideDynamicContent(
+ *        withBasePath('custom/base/path'),
+ *        withCmsAssetsPath('custom/cms/assets/path'),
+ *      )
+ *    ]
+ *  }
+ * );
+ */
+export function provideDynamicContent(...features: DynamicContentFeatures[]) {
+  const providers: (Provider | EnvironmentProviders)[] = [
+    DynamicContentService
+  ];
+
+  const basePathFeature = features.find((f) => f.ɵkind === 'base-path') ?? withBasePath(getDynamicContent);
+  providers.push(basePathFeature.ɵproviders);
+
+  const cmsAssetsPathFeature = features.find((f) => f.ɵkind === 'cms-assets-path') ?? withCmsAssetsPath(getCmsAssets);
+  providers.push(cmsAssetsPathFeature.ɵproviders);
+
+  return makeEnvironmentProviders(providers);
 }
