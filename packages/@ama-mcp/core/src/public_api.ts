a0f9c0b8ca6e65a27acac3c29b7d73f859f13eee
matthieu-crouzet <matthieu.crouzet@amadeus.com>
2025-12-08 16:44:10 +0100

    feat: set up telemetry for mcp server

--- packages/@ama-mcp/core/src/public_api.ts
+++ packages/@ama-mcp/core/src/public_api.ts
@@ -1,5 +1,6 @@
 export * from './cache/index';
 export * from './logger/index';
 export * from './resource/index';
+export * from './server/index';
 export * from './testing/index';
 export * from './tool/index';
