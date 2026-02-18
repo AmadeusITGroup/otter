74e321c3798deaa2fe666ad87cbd3f9902241f87
Kilian Panot <kilian.panot@amadeus.com>
2026-02-18 21:56:31 +0900

    chore(ama-openapi-core): setup vitest for .mts tests

--- packages/@ama-openapi/core/src/schema/configurations/transform-definitions.spec.ts
+++ packages/@ama-openapi/core/src/schema/configurations/transform-definitions.spec.ts
@@ -1,4 +1,12 @@
 import {
+  beforeEach,
+  describe,
+  expect,
+  it,
+  type MockedFunction,
+  vi,
+} from 'vitest';
+import {
   generateModelNameRef,
   getMaskFileName,
 } from '../generate-model-name.mjs';
@@ -6,19 +14,19 @@ import {
   getTransformDefinitions,
 } from './transform-definitions.mjs';
 
-jest.mock('../generate-model-name.mjs', () => ({
-  generateModelNameRef: jest.fn(),
-  getMaskFileName: jest.fn()
+vi.mock('../generate-model-name.mjs', () => ({
+  generateModelNameRef: vi.fn(),
+  getMaskFileName: vi.fn()
 }));
 
 describe('getTransformDefinitions', () => {
   const generateModelNameRefMock =
-    generateModelNameRef as jest.MockedFunction<typeof generateModelNameRef>;
+    generateModelNameRef as MockedFunction<typeof generateModelNameRef>;
   const getMaskFileNameMock =
-    getMaskFileName as jest.MockedFunction<typeof getMaskFileName>;
+    getMaskFileName as MockedFunction<typeof getMaskFileName>;
 
   beforeEach(() => {
-    jest.resetAllMocks();
+    vi.resetAllMocks();
   });
 
   it('should generate transform definitions for each valid model using helpers', () => {
---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import {
+  defineConfig,
+} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    reporters: [
+      'default',
+      'github-actions',
+      'junit'
+    ],
+    outputFile: {
+      junit: './dist-test/junit.xml'
+    },
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      reporter: ['cobertura', 'lcov']
+    }
+  }
+});
