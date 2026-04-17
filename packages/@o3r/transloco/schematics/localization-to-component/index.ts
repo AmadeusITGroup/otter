c4bfec85d85dcfb1da7c53fc29f1e4d8a007756c
Salome DO <salome.do@amadeus.com>
2026-04-17 12:08:49 +0200

    feat: transloco package - schematics

---
+++ packages/@o3r/transloco/schematics/localization-to-component/index.ts
@@ -0,0 +1,399 @@
+import {
+  dirname,
+  posix,
+} from 'node:path';
+import {
+  apply,
+  chain,
+  externalSchematic,
+  MergeStrategy,
+  mergeWith,
+  move,
+  noop,
+  renameTemplateFiles,
+  Rule,
+  schematic,
+  SchematicContext,
+  template,
+  Tree,
+  url,
+} from '@angular-devkit/schematics';
+import {
+  addCommentsOnClassProperties,
+  addImportsAndCodeBlockStatementAtSpecInitializationTransformerFactory,
+  addImportsRule,
+  addInterfaceToClassTransformerFactory,
+  applyEsLintFix,
+  askConfirmationToConvertComponent,
+  createOtterSchematic,
+  generateBlockStatementsFromString,
+  generateClassElementsFromString,
+  getComponentBaseFileName,
+  getComponentTranslationName,
+  getO3rComponentInfoOrThrowIfNotFound,
+  isO3rClassComponent,
+  NoOtterComponent,
+  O3rCliError,
+  sortClassElement,
+} from '@o3r/schematics';
+import {
+  addProviderToModule,
+  insertImport,
+} from '@schematics/angular/utility/ast-utils';
+import {
+  applyToUpdateRecorder,
+  InsertChange,
+} from '@schematics/angular/utility/change';
+import * as ts from 'typescript';
+import type {
+  NgAddLocalizationSchematicsSchema,
+} from './schema';
+
+const localizationProperties = [
+  'translations'
+];
+
+const checkLocalization = (componentPath: string, tree: Tree, baseFileName: string) => {
+  const files = [
+    posix.join(dirname(componentPath), `${baseFileName}-localization.json`),
+    posix.join(dirname(componentPath), `${baseFileName}-translation.ts`)
+  ];
+  if (files.some((file) => tree.exists(file))) {
+    throw new O3rCliError(`Unable to add localization to this component because it already has at least one of these files: ${files.join(', ')}.`);
+  }
+
+  const componentSourceFile = ts.createSourceFile(
+    componentPath,
+    tree.readText(componentPath),
+    ts.ScriptTarget.ES2020,
+    true
+  );
+  const o3rClassDeclaration = componentSourceFile.statements.find((statement): statement is ts.ClassDeclaration =>
+    ts.isClassDeclaration(statement)
+    && isO3rClassComponent(statement)
+  )!;
+  if (o3rClassDeclaration.members.some((classElement) =>
+    ts.isPropertyDeclaration(classElement)
+    && ts.isIdentifier(classElement.name)
+    && localizationProperties.includes(classElement.name.escapedText.toString())
+  )) {
+    throw new O3rCliError(`Unable to add localization to this component because it already has at least one of these properties: ${localizationProperties.join(', ')}.`);
+  }
+};
+
+/**
+ * Add localization architecture to an existing component
+ * @param options
+ */
+export function ngAddLocalizationFn(options: NgAddLocalizationSchematicsSchema): Rule {
+  return async (tree: Tree, context: SchematicContext) => {
+    try {
+      const baseFileName = getComponentBaseFileName(options.path);
+      const { selector, standalone, templateRelativePath } = getO3rComponentInfoOrThrowIfNotFound(tree, options.path);
+
+      checkLocalization(options.path, tree, baseFileName);
+
+      const properties = {
+        ...options,
+        componentTranslation: getComponentTranslationName(baseFileName),
+        componentSelector: selector,
+        name: baseFileName
+      };
+
+      const createLocalizationFilesRule: Rule = mergeWith(apply(url('./templates'), [
+        template(properties),
+        renameTemplateFiles(),
+        move(dirname(options.path))
+      ]), MergeStrategy.Overwrite);
+
+      const updateComponentRule: Rule = chain([
+        addImportsRule(options.path, [
+          {
+            from: '@angular/core',
+            importNames: [
+              'Input'
+            ]
+          },
+          {
+            from: '@o3r/transloco',
+            importNames: [
+              'Localization',
+              'Translatable'
+            ]
+          },
+          {
+            from: `./${properties.name}-translation`,
+            importNames: [
+              'translations',
+              properties.componentTranslation
+            ]
+          }
+        ]),
+        () => {
+          const componentSourceFile = ts.createSourceFile(
+            options.path,
+            tree.readText(options.path),
+            ts.ScriptTarget.ES2020,
+            true
+          );
+          const result = ts.transform(componentSourceFile, [
+            addInterfaceToClassTransformerFactory(`Translatable<${properties.componentTranslation}>`, isO3rClassComponent),
+            (ctx) => (rootNode: ts.Node) => {
+              const { factory } = ctx;
+              const visit = (node: ts.Node): ts.Node => {
+                if (ts.isClassDeclaration(node) && isO3rClassComponent(node)) {
+                  const propertiesToAdd = generateClassElementsFromString(`
+              @Input()
+              public translations: ${properties.componentTranslation};
+            `);
+                  const constructorDeclaration = node.members.find((classElement): classElement is ts.ConstructorDeclaration => ts.isConstructorDeclaration(classElement));
+
+                  const localizationConstructorBlockStatements = generateBlockStatementsFromString('this.translations = translations;');
+
+                  const newContructorDeclaration = constructorDeclaration
+                    ? factory.updateConstructorDeclaration(
+                      constructorDeclaration,
+                      ts.getModifiers(constructorDeclaration) || [],
+                      constructorDeclaration.parameters,
+                      constructorDeclaration.body
+                        ? factory.updateBlock(
+                          constructorDeclaration.body, constructorDeclaration.body.statements.concat(localizationConstructorBlockStatements)
+                        )
+                        : factory.createBlock(localizationConstructorBlockStatements, true)
+                    )
+                    : factory.createConstructorDeclaration(
+                      [],
+                      [],
+                      factory.createBlock(localizationConstructorBlockStatements, true)
+                    );
+
+                  const newModifiers = ([] as ts.ModifierLike[])
+                    .concat(ts.getDecorators(node) || [])
+                    .concat(ts.getModifiers(node) || []);
+
+                  const newMembers = node.members
+                    .filter((classElement) => !ts.isConstructorDeclaration(classElement))
+                    .concat(propertiesToAdd, newContructorDeclaration)
+                    .toSorted(sortClassElement);
+
+                  addCommentsOnClassProperties(
+                    newMembers,
+                    {
+                      translations: 'Localization of the component'
+                    }
+                  );
+
+                  return factory.updateClassDeclaration(
+                    node,
+                    newModifiers,
+                    node.name,
+                    node.typeParameters,
+                    node.heritageClauses,
+                    newMembers
+                  );
+                }
+                return ts.visitEachChild(node, visit, ctx);
+              };
+              return ts.visitNode(rootNode, visit);
+            }
+          ]);
+
+          const printer = ts.createPrinter({
+            removeComments: false,
+            newLine: ts.NewLineKind.LineFeed
+          });
+
+          tree.overwrite(options.path, printer.printFile(result.transformed[0] as any as ts.SourceFile));
+
+          const sf = ts.createSourceFile(
+            options.path,
+            tree.readText(options.path),
+            ts.ScriptTarget.ES2020,
+            true
+          );
+
+          // Has to be done at the end because ts.Printer as some issues with Decorators with arguments
+          const translationsPropDeclaration = sf.statements
+            .find((statement): statement is ts.ClassDeclaration => ts.isClassDeclaration(statement) && isO3rClassComponent(statement))!
+            .members.find((member): member is ts.PropertyDeclaration => ts.isPropertyDeclaration(member) && member.name.getText() === 'translations')!;
+          const translationsPropLastDecorator = [...(ts.getDecorators(translationsPropDeclaration) || [])].at(-1)!;
+          tree.commitUpdate(
+            tree
+              .beginUpdate(options.path)
+              .insertRight(
+                translationsPropLastDecorator.getEnd(),
+                `\n  @Localization('./${baseFileName}-localization.json')`
+              )
+          );
+
+          return tree;
+        }
+      ]);
+
+      const updateTemplateRule: Rule = () => {
+        const templatePath = templateRelativePath && posix.join(dirname(options.path), templateRelativePath);
+        if (templatePath && tree.exists(templatePath)) {
+          tree.commitUpdate(
+            tree
+              .beginUpdate(templatePath)
+              .insertLeft(0, '<div>Localization: {{ translations.dummyLoc1 | o3rTranslate }}</div>\n')
+          );
+        }
+
+        return tree;
+      };
+
+      const specFilePath = options.specFilePath || posix.join(dirname(options.path), `${baseFileName}.spec.ts`);
+      const updateSpecRule: Rule = chain([
+        addImportsRule(specFilePath, [
+          {
+            from: '@angular/core',
+            importNames: ['Provider']
+          },
+          {
+            from: '@o3r/transloco',
+            importNames: ['LocalizationService']
+          },
+          {
+            from: '@o3r/testing/transloco',
+            importNames: ['provideLocalizationMock']
+          }
+        ]),
+        () => {
+          if (!tree.exists(specFilePath)) {
+            context.logger.warn(`No update applied on spec file because ${specFilePath} does not exist.`);
+            return;
+          }
+
+          let specSourceFile = ts.createSourceFile(
+            specFilePath,
+            tree.readText(specFilePath),
+            ts.ScriptTarget.ES2020,
+            true
+          );
+
+          const recorder = tree.beginUpdate(specFilePath);
+
+          const lastImport = [...specSourceFile.statements].reverse().find((statement) =>
+            ts.isImportDeclaration(statement)
+          );
+
+          const changes = [new InsertChange(specFilePath, lastImport?.getEnd() || 0, `
+const localizationConfiguration = {language: 'en'};
+const mockTranslations = {
+  en: {${options.activateDummy
+    ? `
+    '${properties.componentSelector}.dummyLoc1': 'Dummy 1'
+  `
+    : ''}}
+};
+      `)];
+
+          applyToUpdateRecorder(recorder, changes);
+          tree.commitUpdate(recorder);
+
+          specSourceFile = ts.createSourceFile(
+            specFilePath,
+            tree.readText(specFilePath),
+            ts.ScriptTarget.ES2020,
+            true
+          );
+
+          const result = ts.transform(specSourceFile, [
+            (ctx) => addImportsAndCodeBlockStatementAtSpecInitializationTransformerFactory(
+              [
+                ctx.factory.createSpreadElement(ctx.factory.createCallExpression(
+                  ctx.factory.createIdentifier('provideLocalizationMock'),
+                  undefined,
+                  [
+                    ctx.factory.createIdentifier('localizationConfiguration'),
+                    ctx.factory.createIdentifier('mockTranslations')
+                  ]
+                ))
+              ],
+              `const localizationService = TestBed.inject(LocalizationService);\nlocalizationService.configure();\n`
+            )(ctx)
+          ]);
+
+          const printer = ts.createPrinter({
+            removeComments: false,
+            newLine: ts.NewLineKind.LineFeed
+          });
+
+          const newContent = printer.printFile(result.transformed[0] as any as ts.SourceFile);
+
+          tree.overwrite(specFilePath, newContent);
+
+          return tree;
+        }
+      ]);
+
+      const updateModuleRule: Rule = () => {
+        const moduleFilePath = options.path.replace(/\.ts$/, '-module.ts');
+
+        // Check if module file exists
+        if (!tree.exists(moduleFilePath)) {
+          return tree;
+        }
+
+        const moduleSourceFile = ts.createSourceFile(
+          moduleFilePath,
+          tree.readText(moduleFilePath),
+          ts.ScriptTarget.ES2020,
+          true
+        );
+
+        // Check if provideLocalization is already in the module
+        const moduleContent = tree.readText(moduleFilePath);
+        if (moduleContent.includes('provideLocalization')) {
+          return tree;
+        }
+
+        const recorder = tree.beginUpdate(moduleFilePath);
+        const importChanges = insertImport(moduleSourceFile, moduleFilePath, 'provideLocalization', '@o3r/transloco');
+        if (importChanges instanceof InsertChange) {
+          recorder.insertLeft(importChanges.pos, importChanges.toAdd);
+        }
+        const providerChanges = addProviderToModule(moduleSourceFile, moduleFilePath, 'provideLocalization()', '@o3r/transloco');
+        applyToUpdateRecorder(recorder, providerChanges);
+        tree.commitUpdate(recorder);
+      };
+
+      const addDummyKeyRule: Rule = schematic('add-localization-key', {
+        path: options.path,
+        skipLinter: options.skipLinter,
+        key: 'dummyLoc1',
+        description: 'Dummy 1 description',
+        value: 'Dummy 1'
+      });
+
+      return chain([
+        createLocalizationFilesRule,
+        updateComponentRule,
+        updateSpecRule,
+        standalone ? noop() : updateModuleRule,
+        ...(options.activateDummy ? [addDummyKeyRule, updateTemplateRule] : []),
+        options.skipLinter ? noop() : applyEsLintFix()
+      ]);
+    } catch (e) {
+      if (e instanceof NoOtterComponent && context.interactive) {
+        const shouldConvertComponent = await askConfirmationToConvertComponent();
+        if (shouldConvertComponent) {
+          return chain([
+            externalSchematic('@o3r/core', 'convert-component', {
+              path: options.path
+            }),
+            ngAddLocalizationFn(options)
+          ]);
+        }
+      }
+      throw e;
+    }
+  };
+}
+
+/**
+ * Add localization architecture to an existing component
+ * @param options
+ */
+export const ngAddLocalization = createOtterSchematic(ngAddLocalizationFn);
---
+++ packages/@o3r/transloco/schematics/ng-add/index.ts
@@ -0,0 +1,81 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import {
+  chain,
+  noop,
+  Rule,
+} from '@angular-devkit/schematics';
+import {
+  applyEsLintFix,
+  createOtterSchematic,
+  ngAddDependenciesRule,
+  registerPackageCollectionSchematics,
+  setupSchematicsParamsForProject,
+} from '@o3r/schematics';
+import type {
+  PackageJson,
+} from 'type-fest';
+import {
+  updateCmsAdapter,
+} from '../cms-adapter';
+import {
+  updateI18n,
+  updateLocalization,
+} from '../localization-base';
+import {
+  registerDevtools,
+} from './helpers/devtools-registration';
+import type {
+  NgAddSchematicsSchema,
+} from './schema';
+
+/**
+ * List of external dependencies to be added to the project as peer dependencies
+ */
+const dependenciesToInstall = [
+  '@angular/cdk',
+  '@angular/common',
+  '@angular/core',
+  '@jsverse/transloco',
+  '@ngrx/store',
+  'rxjs'
+];
+
+/**
+ * List of external dependencies to be added to the project as dev dependencies
+ */
+const devDependenciesToInstall = [
+  '@angular-devkit/core',
+  'chokidar',
+  'globby'
+];
+
+const packageJsonPath = path.resolve(__dirname, '..', '..', 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, { encoding: 'utf8' })) as PackageJson;
+
+/**
+ * Add Otter localization (Transloco) to an Angular Project
+ * @param options for the dependencies installations
+ */
+function ngAddFn(options: NgAddSchematicsSchema): Rule {
+  return chain([
+    (_, context) => {
+      context.logger.info(`The package @o3r/transloco comes with a debug mechanism`);
+      context.logger.info('Get information on https://github.com/AmadeusITGroup/otter/tree/main/docs/localization/LOCALIZATION.md#Debugging');
+    },
+    updateLocalization(options, __dirname),
+    updateI18n(options),
+    options.skipLinter ? noop() : applyEsLintFix(),
+    ngAddDependenciesRule(options, packageJsonPath, { dependenciesToInstall, devDependenciesToInstall }),
+    updateCmsAdapter(options),
+    registerPackageCollectionSchematics(packageJson),
+    setupSchematicsParamsForProject({ '@o3r/core:component*': { useLocalization: true } }, options.projectName),
+    registerDevtools(options)
+  ]);
+}
+
+/**
+ * Add Otter localization (Transloco) to an Angular Project
+ * @param options for the dependencies installations
+ */
+export const ngAdd = (options: NgAddSchematicsSchema) => createOtterSchematic(ngAddFn)(options);
