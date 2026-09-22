bda15bbc5c20ca7e5646fc5a1ba0e43dd7459724
Salome DO <salome.do@amadeus.com>
2026-09-22 14:54:09 +0200

    fix: middlewares of configuration and dynamic-content

--- packages/@o3r/configuration/middlewares/bootstrap-config-middleware.ts
+++ packages/@o3r/configuration/middlewares/bootstrap-config-middleware.ts
@@ -3,7 +3,7 @@
 import * as cheerio from 'cheerio';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- no type are provided for express-interceptor
 // @ts-ignore
-import * as expressInterceptor from 'express-interceptor';
+import expressInterceptor from 'express-interceptor';
 
 /**
  * Injects bootstrapconfig inside the body dataset to get all debug/override capabilities on a local development server
